32fdfc6e70023d6fde583ec14dcc42a37609059f
Tushar Khatri <khatritushar320@gmail.com>
2025-09-14 14:35:18 +0530

    Integrate razorpay payment sandbox api and callback

--- go.mod
+++ go.mod
@@ -14,7 +14,9 @@ require (
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.27.0 // indirect
+	github.com/go-resty/resty/v2 v2.16.5 // indirect
 	github.com/goccy/go-json v0.10.5 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/cpuid/v2 v2.3.0 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
@@ -22,6 +24,7 @@ require (
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pelletier/go-toml/v2 v2.2.4 // indirect
+	github.com/razorpay/razorpay-go v1.4.0 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.3.0 // indirect
 	golang.org/x/arch v0.21.0 // indirect
--- main.go
+++ main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"net/http"
 	"os"
 
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 // Data
@@ -71,19 +74,109 @@ func getPaymentHistory(c *gin.Context) {
 }
 
 func main() {
+
+	_ = godotenv.Load()
 	r := gin.Default()
 
-	// Subscriptions
+	// Subscriptions enpoints
 	r.GET("/subscriptions/:userId", getSubscriptions)
 
-	// Billing
+	// Billing endpoints
 	r.GET("/billing/:userId", getBillingInfo)
 	r.POST("/billing/:userId/email", updateBillingEmail)
 
-	// Invoices & Payments
+	// Invoices endpoints
 	r.GET("/invoices/:userId", getInvoices)
 	r.POST("/invoices/:invoiceId/pay", payInvoice)
 	r.GET("/invoices/:userId/history", getPaymentHistory)
+
+	//Payment endpoint
+	r.POST("/api/payments/initiate", func(c *gin.Context) {
+		keyID := os.Getenv("RAZORPAY_KEY_ID")
+		keySecret := os.Getenv("RAZORPAY_SECRET")
+		callbackURL := os.Getenv("CALLBACK_URL")
+
+		if keyID == "" || keySecret == "" || callbackURL == "" {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Missing Razorpay credentials or callback"})
+			return
+		}
+		var req struct {
+			InvoiceID string  `json:"invoice_id"`
+			Amount    float64 `json:"amount"`
+			Method    string  `json:"method"`
+		}
+
+		if err := c.ShouldBindJSON(&req); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid request"})
+			return
+		}
+
+		payload := map[string]interface{}{
+			"amount":         req.Amount * 100, // INR → paise
+			"currency":       "INR",
+			"accept_partial": false,
+			"description":    "Invoice " + req.InvoiceID,
+			"customer": map[string]string{
+				"name":    "Tushar Khatri",
+				"email":   "hello@tusharkhatri.in",
+				"contact": "9968290156",
+			},
+			"notify": map[string]bool{
+				"sms":   true,
+				"email": true,
+			},
+			"callback_url":    callbackURL,
+			"callback_method": "get",
+		}
+
+		body, _ := json.Marshal(payload)
+
+		reqRazor, _ := http.NewRequest("POST", "https://api.razorpay.com/v1/payment_links", bytes.NewBuffer(body))
+		reqRazor.Header.Set("Content-Type", "application/json")
+		reqRazor.SetBasicAuth(keyID, keySecret)
+
+		client := &http.Client{}
+		resp, err := client.Do(reqRazor)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to create payment link"})
+			return
+		}
+		defer resp.Body.Close()
+
+		var result map[string]interface{}
+		if err := json.NewDecoder(resp.Body).Decode(&result); err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to parse Razorpay response"})
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{
+			"payment_link": result["short_url"],
+			"invoice_id":   req.InvoiceID,
+			"amount":       req.Amount,
+			"status":       "link_created",
+		})
+	})
+
+	r.GET("/api/payments/callback", func(c *gin.Context) {
+		paymentID := c.Query("razorpay_payment_id")
+		orderID := c.Query("razorpay_order_id")
+		signature := c.Query("razorpay_signature")
+		status := "failed"
+
+		if paymentID != "" {
+			status = "success"
+		}
+
+		result := gin.H{
+			"payment_id": paymentID,
+			"order_id":   orderID,
+			"signature":  signature,
+			"status":     status,
+			"message":    "Payment callback received",
+		}
+
+		c.JSON(http.StatusOK, result)
+	})
 
 	port := os.Getenv("PORT")
 	if port == "" {
